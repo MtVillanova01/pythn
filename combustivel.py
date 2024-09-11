a2700f2406b27447d81778b3f9252b366805eac1
MtVillanova01 <00001109556639sp@al.educacao.sp.gov.br>
2024-09-11 09:35:10 -0300

    Create combustivel.py

---
+++ combustivel.py
@@ -0,0 +1,7 @@
+distancia = float(input("Distancia Percorrido: "))
+combustivel = float(input("Valor Combustivel: "))
+preço = 5.50
+
+consumo_medio = distancia / (combustivel / preço)
+
+print("Consumo Medio: ",consumo_medio)
