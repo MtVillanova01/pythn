568e18465f973f71091e382d3a0cbf6ded24fa29
MtVillanova01 <00001109556639sp@al.educacao.sp.gov.br>
2024-09-11 09:36:51 -0300

    Create carro alugado.py

---
+++ carro alugado.py
@@ -0,0 +1,8 @@
+km = float(input("KM percorrido pelo carro alugado: "))
+diasL = float(input("quantidades de dias alugado: "))
+
+l = km*0.15
+preco = 60*diasL
+
+print("O valor total do alugel foi: ",l)
+print("O consumo foi de: ",preco)
