296ded529e70caf9ce61f7942dc343b976e6e861
MtVillanova01 <00001109556639sp@al.educacao.sp.gov.br>
2024-09-11 07:52:56 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+a = int(input("digite o primeira numero: "))
+b = int(input("digite o segundo numero: "))
+soma = a+b
+subitração = a-b
+multiplicação = a*b
+divisão = a/b 
+print ("o resultado da soma é: ",soma) 
+print ("o resultado da subitração é: ",subitração) 
+print ("o resultado da multiplicação é: ",multiplicação) 
+print ("o resultado da divisão é: ",divisão)
